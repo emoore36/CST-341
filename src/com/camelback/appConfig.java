f687c11d24e3e47f29d60631c3f61b591da8f994
emoore36 <emoore36@my.gcu.edu>
2019-10-09 21:54:59 -0700

    Update appConfig.java
    Just making some preliminary changes to appConfig before we start the serious stuff.

--- src/com/camelback/appConfig.java
+++ src/com/camelback/appConfig.java
@@ -1,5 +1,38 @@
 package com.camelback;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+import com.camelback.business.BusinessInterface;
+import com.camelback.controllers.WelcomeController;
+
 public class appConfig {
-	// TODO: Copy from assignment3
+
+@Configuration
+public class AppConfig {
+
+	@Bean(name = "welcomeController")
+	public WelcomeController getWelcomeController() {
+		return new WelcomeController();
+	}
+	
+	@Bean(name = "productController")
+	public ProductController getProductController() {
+		return new ProductController();
+	}
+
+	@Bean(name = "userService", initMethod = "init", destroyMethod = "destroy")
+	@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public BusinessInterface getUserService() {
+		return new UserBusinessService(); // interchangeable concrete implementation
+	}
+	
+	@Bean(name = "productService", initMethod = "init", destroyMethod = "destroy")
+	@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public BusinessInterface getProductService() {
+		return new ProductBusinessService(); // interchangeable concrete implementation
+	}
+	
 }
