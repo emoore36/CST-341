3e116e3a7ae4b8ce2b5726387f9a27d811e42d5e
emoore36 <emoore36@my.gcu.edu>
2019-10-13 14:50:02 -0700

    Update appConfig.java
    appConfig.java part 1. The product-related pieces are currently commented out. To be improved (and probably replaced) by Harpreet.

--- src/com/camelback/appConfig.java
+++ src/com/camelback/appConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 
+import com.camelback.beans.User;
 import com.camelback.business.BusinessInterface;
+import com.camelback.business.UserBusinessService;
 import com.camelback.controllers.WelcomeController;
-
-public class appConfig {
 
 @Configuration
 public class AppConfig {
@@ -17,22 +17,24 @@ public class AppConfig {
 	public WelcomeController getWelcomeController() {
 		return new WelcomeController();
 	}
-	
-	@Bean(name = "productController")
-	public ProductController getProductController() {
-		return new ProductController();
-	}
+
+	// @Bean(name = "productController")
+	// public ProductController getProductController() {
+	// return new ProductController();
+	// }
 
 	@Bean(name = "userService", initMethod = "init", destroyMethod = "destroy")
 	@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
-	public BusinessInterface getUserService() {
+	public BusinessInterface<User> getUserService() {
 		return new UserBusinessService(); // interchangeable concrete implementation
 	}
-	
-	@Bean(name = "productService", initMethod = "init", destroyMethod = "destroy")
-	@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
-	public BusinessInterface getProductService() {
-		return new ProductBusinessService(); // interchangeable concrete implementation
-	}
-	
+
+	// @Bean(name = "productService", initMethod = "init", destroyMethod =
+	// "destroy")
+	// @Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
+	// public BusinessInterface getProductService() {
+	// return new ProductBusinessService(); // interchangeable concrete
+	// implementation
+	// }
+
 }
